88ecc9261dbb3dd1877fdb518d7df4d31136c82a
Sai krishna Muddabakthuni <saikrishnamuddabakthuni@Sais-MacBook-Air.local>
2026-05-06 01:11:50 +0530

    this is my first commit

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+a=100
+b=200
+echo "decalre the variables are $a $b"
 echo "the script we are executing is $0"
 
 echo "the value of x is $1"
 
 echo "the value of y is $2"
+
+echo $*
